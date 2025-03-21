6c1eb66106c132d21969fe9fb63d73183c3dd23e
VaibhavRokade1 <rokadevaibhav04@.com>
2025-03-21 05:13:05 -0400

    /partials/Header added Successfully...

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
-import express from 'express';
-import { dbConnect } from "./db/db.js"
-import "dotenv/config"
-const PORT = process.env.PORT
+import express from "express";
+import { dbConnect } from "./db/db.js";
+import "dotenv/config";
+const PORT = process.env.PORT;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 app.listen(PORT, () => {
   dbConnect();
-  console.log(`Server is running on http://localhost:${PORT}`)
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
