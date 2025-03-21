35484dad92d9754c6d2acb75a2f20f9f716be760
Jedhepradip <pradipjedhe69@gmail.com>
2025-03-21 14:02:48 +0530

    added the mongodb url and remove the .env file to gitingnore

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import express from 'express';
+import "dotenv/config"
+import { dbConnect } from './db/db.js';
+const app = express();
 
-const app = express();  
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(3000, () => {    
-    console.log('Server is running on http://localhost:3000');
-    }
+app.listen(3000, () => {
+  dbConnect();
+  console.log('Server is running on http://localhost:3000');
+}
 );  
