1bc59ad993d7cd644ec4d7ea4acad5cdb10a82cd
krushna kasal <krushnakasal97@gmail.com>
2025-03-21 13:47:19 +0530

    created a server with express.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
-// ssnzxm
-// krushna 
-// pradip 
+import express from 'express';
+
+const app = express();  
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(3000, () => {    
+    console.log('Server is running on http://localhost:3000');
+    }
+);  
