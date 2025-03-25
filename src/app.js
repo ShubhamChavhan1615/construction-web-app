dab711946176e65cc024ce508236b3c57fbc2c72
Jedhepradip <pradipjedhe69@gmail.com>
2025-03-25 12:16:57 +0530

    ..

--- src/app.js
+++ src/app.js
@@ -15,6 +15,8 @@ app.get("/", (req, res) => {
   res.render("index", { title: "Home" });
 });
 
+app.use("api-user", UserRout)
+
 app.get("/About", (req, res) => {
   res.render("About", { title: "About" });
 });
