1722c0bf9759743c91612eaa1ae6ff413af37ea0
VaibhavRokade1 <rokadevaibhav04@.com>
2025-03-25 02:51:20 -0400

    Login Page added

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import { dbConnect } from "./db/db.js";
 import "dotenv/config";
 const PORT = process.env.PORT;
 const app = express();
-import UserRout from "./routes/user.js"
+import UserRout from "./routes/user.js";
 
 Router();
 app.set("view engine", "ejs");
@@ -15,10 +15,14 @@ app.get("/", (req, res) => {
   res.render("index", { title: "Home" });
 });
 
-app.use("api-user", UserRout)
+app.use("api-user", UserRout);
 
 app.get("/About", (req, res) => {
   res.render("About", { title: "About" });
+});
+
+app.get("/Login", (req, res) => {
+  res.render("Login");
 });
 
 app.listen(PORT, () => {
