792f43d8d04a230c3785e19ed77d81fb29c0c836
Jedhepradip <pradipjedhe69@gmail.com>
2025-03-26 12:49:30 +0530

    added palse models

---
+++ src/models/plans.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const ProjectSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["Ongoing", "Completed", "Pending"],
+        required: true
+    },
+    price: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 5
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    endDate: {
+        type: Date,
+        required: true
+    },
+    image: {
+        type: [String],
+        required: true
+    },
+});
+
+module.exports = mongoose.model("Project", ProjectSchema);
