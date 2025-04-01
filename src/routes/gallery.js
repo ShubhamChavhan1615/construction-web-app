37d7a845937f2f1c8259c006fc6e9d3c817f1260
krushna kasal <krushnakasal97@gmail.com>
2025-04-01 14:55:53 +0530

    .....

--- src/routes/gallery.js
+++ src/routes/gallery.js
@@ -1,10 +1,9 @@
 import express from "express"
 import { getGallary, Creategallery, Deletegallary } from "../controllers/gallary.controles.js"
 import { checkAuth } from "../middleware/jwt.middleware.js"
-import  upload  from "../middleware/Multer_Middleware.js"
 const Router = express.Router()
 
-Router.post("/insert-gallaryimg", upload.single("galleryImage"), Creategallery)
+Router.post("/insert-gallaryimg", Creategallery)
 Router.get("/get-gallaryimg", checkAuth, getGallary)
 Router.delete("/delete-gallaryimg/:id", checkAuth, Deletegallary)
 
